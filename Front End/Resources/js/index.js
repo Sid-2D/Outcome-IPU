e051a22b8026ffb936983c706f6f2e756b5953b5
Siddharth Chopra <sid6327@gmail.com>
2017-03-07 11:43:47 +0530

    Adding table layout and click method in JS.

---
+++ Front End/Resources/js/index.js
@@ -0,0 +1,49 @@
+var tableTemplate = [
+    // 0 h3 Name
+    {
+        "tag": "h3",
+        "id": "Name",
+        "style": "color: #999; animation: fadeIn 4s",
+        "text": ""
+    },
+    // 1 h4 Sem
+    {
+        "tag": "h4",
+        "id": "Sem",
+        "style": "color:#999; animation: fadeIn 7s; text-align: left; margin-left: 5px",
+        "text": ""
+    },
+    // 2 div-table
+    {
+        "tag": "div",
+        "class": "table-responsive"
+    },
+    // 3 table
+    {
+        "tag": "table",
+        "class": "table",
+        "style": "color: #fff; animation: fadeIn 10s"
+    },
+    // 4 div resultFooter
+    {
+        "tag": "div",
+        "id": "resultFooter",
+        "style": "animation: fadeIn 7s;"
+    },
+    // 5 h4 Total
+    {
+        "tag": "h4",
+        "id": "Total",
+        "style": "color:#999; text-align: left; margin-left: 5px",
+        "text": ""
+    },
+    // 6 hr
+    {
+        "tag": "hr",
+        "style": "margin: 0px; background-color: #999; width: 100%;"
+    }
+];
+
+function findResult() {
+    
+}
