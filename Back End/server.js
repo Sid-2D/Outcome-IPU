a0427d394a4cd88a1653866fc86763fc05a0e71b
Siddharth Chopra <sid6327@gmail.com>
2017-03-13 02:26:17 +0530

    Adding code and logic for rank lists.

--- Back End/server.js
+++ Back End/server.js
@@ -2,7 +2,7 @@ var app = require('express')(),
 	MongoClient = require('mongodb').MongoClient,
 	bodyParser = require('body-parser'),
 	path = require('path');
-
+	
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
@@ -12,17 +12,36 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/../Front End/index.html'));
+	res.sendFile(path.join(__dirname + '/../Front End/debug.html'));
 });
 
 app.get('/:roll', function (req, res) {
 	MongoClient.connect(process.env.MONGO_URL, function (err, db) {
 		if (err) {
-			db.close();
 			return res.send(err);			
 		}
 		db.collection('Student').find({'EnrollmentNumber': req.params['roll']}).toArray(function (err, docs) {
+			if (err) {
+				db.close();
+				return res.send(err);
+			}
 			res.send(docs);
+			db.close();
+		});
+	});
+});
+
+app.post('/rank', function (req, res) {
+	MongoClient.connect(process.env.MONGO_URL, function (err, db) {
+		if (err) {
+			return res.send(err);
+		}
+		db.collection('Analyze').findOne({_id: req.body}, function (err, doc) {
+			if (err) {
+				db.close();
+				return res.send(err);				
+			}
+			res.send(doc);
 			db.close();
 		});
 	});
