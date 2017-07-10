2bec11662a4ec02d3b1b0c5950ec65a342eda7b5
Sid <sid6327@gmail.com>
2017-07-10 22:45:41 +0530

    Adding helper scripts for subject names and aggregates.

---
+++ Parser/HelperScripts/setMarks.js
@@ -0,0 +1,68 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(process.env.MLAB, function (err, db) {
+	if (err) {
+		console.log("Crashed at init:");
+		console.log(err);
+		process.exit();
+	}
+	db.collection('Student').aggregate([
+		{ $match: { Score: null } },
+		{ $unwind: "$Marks" },
+		{ $lookup: {
+          from: "Subject",
+          localField: "Marks.Id",
+          foreignField: "_id",
+          as: "Sub"
+        } },
+        { $unwind: "$Sub" },
+		{ $group: {
+			_id: "$_id", 
+			Marks: { $push: "$Marks" },
+			Names: { $push: "$Sub" },
+			Examination: { $first: "$Examination" },
+			EnrollmentNumber: { $first: "$EnrollmentNumber" }
+		} },
+		{ $project: {
+			_id: 1,
+			Marks: 1,
+			Names: 1,
+			Examination: 1,
+			EnrollmentNumber: 1,
+			Score: { $sum: "$Marks.Total" }
+		} }
+	]).toArray(function (err, studentArray) {
+		if (err) {
+			console.log("Crashed at aggregate:");
+			console.log(err);
+			db.close();
+			process.exit();
+		}
+		studentArray.forEach(function (student, index) {
+			// Update the student record with Subject names and score
+			var updates = {};
+			updates['Marks'] = student.Marks;
+			for (let i = 0; i < student.Marks.length; i++) {
+				updates['Marks'][i]['Name'] = student.Names[i].Name;
+			}
+			if (/regular/gi.test(student.Examination)) {
+				updates['Score'] = Number((student.Score / student.Marks.length).toFixed(2)); 
+			} else {
+				updates['Score'] = '-';
+			}
+			updates['CollegeCode'] = student.EnrollmentNumber.substr(3, 3);
+			db.collection('Student').update({_id: student['_id']}, { $set: updates }, function (err) {
+				if (err) {
+					console.log("Crashed at update:");
+					console.log(err);
+					db.close();
+					process.exit();
+				}
+				if (index === studentArray.length - 1) {
+					console.log("Done.");
+					db.close();
+				}
+			});	
+		});
+	});
+});
---
+++ Parser/HelperScripts/setRanks.js
@@ -0,0 +1,36 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(process.env.MLAB, function (err, db) {
+	if (err) {
+		console.log("Crashed at init:");
+		console.log(err);
+		process.exit();
+	}
+	db.collection("Student").aggregate([
+		{ $match: { Examination: { $regex: /regular/gi } } },
+		{ $group: {
+			_id: { Examination: "$Examination", Semester: "$Semester", Programme: "$Programme", Batch: "$Batch", CollegeCode: "$CollegeCode" },
+			Students: { $push: { Name: "$Name", EnrollmentNumber: "$EnrollmentNumber", Scores: "$Score" } },
+		} },
+		{ $unwind: "$Students" },
+		{ $sort: { "Students.Scores": -1 } },
+		{ $group: {
+			_id: "$_id",
+			Students: { $push: "$Students" },
+		} },
+		{ $project: { 
+			Students: 1,
+			Aggregate: {$sum: "$Students.Scores" }
+		} },
+		{ $out: "Analyze" }
+	]).toArray(function (err) {
+		if (err) {
+			console.log("Crashed at Aggregate:");
+			console.log(err);
+			db.close();
+			process.exit();
+		}
+		console.log("Done.");
+		db.close();
+	});
+});
