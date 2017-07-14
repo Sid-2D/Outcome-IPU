20c0337528f318f8d6dc25a3ac10db82ed0fbb72
Sid <sid6327@gmail.com>
2017-07-14 23:29:14 +0530

    Adapting code to linux.

--- Parser/Student-Parser.js
+++ Parser/Student-Parser.js
@@ -1,24 +1,40 @@
-var Sem, Name, Batch, Examination, final = [], len = 0;	// Globals for same set of students.
+var os = require('os');
 
-module.exports = function (data, db, cb) {
-	var regexForStudents = /\n\d{11}([^]*?)\n\w\w?\*?(\(..?\))?\r\n\w\w?\r/g;
-	require('fs').writeFileSync('data.json', JSON.stringify(data, null, 2));
-	process.exit();
+var Sem, Name, Batch, Examination, Institution, InstitutionCode, final = [], len = 0;	// Globals for same set of students.
+var currentSubjects = [];	// Stores the names and subject codes for current set of students.
+var previousResult = {};
+
+module.exports = function (data, subjectArray, db, cb) {
+	var regexForStudentsLinux = /\n\d{11}([^]*?)\n\w\w?\*?(\(..?\))?\n\w\w?\n/g;
+	var regexForStudentsWindows = /\n\d{11}([^]*?)\n\w\w?\*?(\(..?\))?\r\n\w\w?\r/g;
+	var regexForStudents;
+	if (os.platform() === 'linux') {
+		regexForStudents = regexForStudentsLinux;
+	} else {
+		regexForStudents = regexForStudentsWindows;
+	}
 	data.forEach((students) => {
 		var studentList = students.match(regexForStudents);
 		if (studentList) {
-			studentList = studentList.map((ele) => (ele.split('\r\n')));
+			studentList = studentList.map((ele) => (ele.split(os.EOL)));
 			Sem = students.match(/Sem\.\/Year: (\d+)/)[1];
 			Name = students.match(/Programme Name: ([^]*?) Sem/)[1];
 			if (students.match(/Batch: ([^]*?) Exa/)) {
 				Batch = students.match(/Batch: ([^]*?) Exa/)[1];
-				if (parseInt(Batch) < 2013) {
-					return;
-				}				
+				// if (parseInt(Batch) < 2013) {
+				// 	return;
+				// }				
 			} else {
 				return;
 			}
-			Examination = students.match(/Examination: ([^]*?)\r\n/)[1];
+			// Institution = students.match(/Institution: ([^]*?)\r/)[1];
+			Institution = students.match(new RegExp('Institution: ([^]*?)' + os.EOL))[1];
+			InstitutionCode = students.match(/Institution Code: (\d+)/)[1];
+			// Examination = students.match(/Examination: ([^]*?)\r\n/)[1];
+			Examination = students.match(new RegExp('Examination: ([^]*?)' + os.EOL))[1];
+			if (matchSubjectList()) {
+				currentSubjects = subjectArray.shift();
+			}
 			studentList.forEach((student) => {
 				var obj = studentBlueprint();
 				obj.EnrollmentNumber = student.shift().substr(1);
@@ -36,6 +52,7 @@ module.exports = function (data, db, cb) {
 					var marks = marksBlueprint();
 					var id = student.shift();
 					marks.Id = id.match(/\w+/)[0];
+					marks.Name = getNameFromCurrentList(marks.Id);
 					try {
 						marks.Credits = id.match(/\((\w+)\)/)[1];
 					} catch (e) {  }
@@ -58,11 +75,11 @@ module.exports = function (data, db, cb) {
 				if (!present) {
 					return;
 				}
-				db.collection('Student').insert(obj, function (err) {
-					if (err) {
-						console.error(err);
-					}
-				});
+				// db.collection('Student').insert(obj, function (err) {
+				// 	if (err) {
+				// 		console.error(err);
+				// 	}
+				// });
 				final[len++] = obj;
 			});
 		}	
@@ -76,6 +93,8 @@ var studentBlueprint = function () {
 		'Programme': Name,
 		'Batch': Batch,
 		'Examination': Examination,
+		'Institution': Institution,
+		'Institution Code': InstitutionCode,
 		'EnrollmentNumber': null,
 		'Name': null,
 		'Marks': [],
@@ -91,5 +110,24 @@ var marksBlueprint = function () {
 		'External': null,
 		'Total': null,
 		'Grade': null
+	}
+}
+
+var matchSubjectList = function () {
+	var currentResult = studentBlueprint();
+	if (JSON.stringify(currentResult) === JSON.stringify(previousResult)) {
+		return false;
+	}
+	previousResult = currentResult;
+	return true;
+}
+
+var getNameFromCurrentList = function (id) {
+	console.log('Required', id)
+	for (var i = 0; i < currentSubjects.length; i++) {
+		console.log('Current', currentSubjects[i]['_id'])
+		if (currentSubjects[i]['_id'] === id) {
+			return currentSubjects[i]['Name'];
+		}
 	}
 }
