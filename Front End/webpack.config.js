24a6be5fb9d66874415677d29ab8df2431635534
Sid <sid6327@gmail.com>
2017-07-17 23:50:09 +0530

    Adding css minifier new file tree.

--- Front End/webpack.config.js
+++ Front End/webpack.config.js
@@ -1,16 +1,18 @@
 var path = require('path')
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
 	entry: './Resources/webpack.entry.js',
 	output: {
-		path: path.resolve(__dirname, 'Resources'),
-		filename: 'webpack.bundle.js'
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
 	},
 	module: {
 		rules: [
-			{ test: /\.css$/, loader: 'style-loader!css-loader' },
+			{ test: /\.css$/, loader: ExtractTextPlugin.extract('css-loader') },
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
@@ -21,11 +23,13 @@ module.exports = {
 		]
 	},
 	plugins: [
-		new webpack.optimize.UglifyJsPlugin(),
-		new HtmlWebpackPlugin({ 
-			filename: 'index_bundle1.html',
-			template: 'index_bundle.html',
-			inject: 'head'
-		})
+		new ExtractTextPlugin('bundle.css'),
+		// new OptimizeCSSAssetsPlugin(),
+		new webpack.optimize.UglifyJsPlugin()
+		// new HtmlWebpackPlugin({ 
+		// 	filename: 'index.html',
+		// 	template: 'index_template.html',
+		// 	inject: 'head'
+		// })
 	]
 }
