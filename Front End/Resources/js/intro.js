0089a576fb11f8e769ddc3001989a15042a14f2b
Sid <sid6327@gmail.com>
2018-03-17 23:33:26 +0530

    Adding code for sidebar, overlay and history API.

---
+++ Front End/Resources/js/intro.js
@@ -0,0 +1,15 @@
+let intro = '<br> \
+			<div class="row" id="intro1" style="opacity: 0; padding: 5px 1px;"> \
+                <div class="col-md-4 offset-md-4" style="color:#ccc; text-align: center; font-size: 20px;">CS and ECE results</div> \
+            </div> \
+            <div class="row" id="intro2" style="opacity: 0; padding: 5px 1px;"> \
+                <div class="col-md-4 offset-md-4" style=" color:#ccc; text-align: center; font-size: 20px;">Downloadable reports</div> \
+            </div> \
+            <div class="row" id="intro3" style="opacity: 0; padding: 5px 1px;"> \
+                <div class="col-md-4 offset-md-4" style=" color:#ccc; text-align: center; font-size: 20px;">Ad-free and open source</div> \
+            </div> \
+            <div class="row" id="intro4" style="opacity: 0; padding: 5px 1px;"> \
+                <div class="col-md-6 offset-md-3" style=" color:#ccc; text-align: center; font-size: 20px;"><span style="cursor: pointer;" onclick="sampleResult()">Not a student? Click <u>here</u> for a sample result</span></div> \
+            </div>';
+
+module.exports = intro;
