94d076366bf0a8d10caa56632b06286b0a90d012
Sid <sid6327@gmail.com>
2018-04-09 23:17:49 +0530

    Updating intro message.

--- Front End/Resources/js/intro.js
+++ Front End/Resources/js/intro.js
@@ -1,12 +1,12 @@
 let intro = '<br> \
 			<div class="row" id="intro1" style="opacity: 0; padding: 5px 1px;"> \
-                <div class="col-md-4 offset-md-4" style="color:#ccc; text-align: center; font-size: 20px;">CS and ECE results</div> \
+                <div class="col-md-4 offset-md-4" style="color:#ccc; text-align: center; font-size: 20px;">CS, IT, EEE, ECE, MAE results</div> \
             </div> \
             <div class="row" id="intro2" style="opacity: 0; padding: 5px 1px;"> \
                 <div class="col-md-4 offset-md-4" style=" color:#ccc; text-align: center; font-size: 20px;">Downloadable reports</div> \
             </div> \
             <div class="row" id="intro3" style="opacity: 0; padding: 5px 1px;"> \
-                    <div class="col-md-4 offset-md-4" style=" color:#ccc; text-align: center; font-size: 20px;">University/Class ranks</div> \
+                    <div class="col-md-4 offset-md-4" style=" color:#ccc; text-align: center; font-size: 20px;">University/College ranks</div> \
             </div> \
             <div class="row" id="intro4" style="opacity: 0; padding: 5px 1px;"> \
                 <div class="col-md-4 offset-md-4" style=" color:#ccc; text-align: center; font-size: 20px;">Ad-free and open source</div> \
